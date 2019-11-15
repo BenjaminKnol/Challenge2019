70d07f23b141249050a238ac88ce0defdf493549
BoasKaken <boas.kalma@planet.nl>
2019-11-15 09:57:13 +0100

    addSources

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,8 +1,10 @@
+//all the loraWAN code is the deafault theThingsNetworkCode: https://github.com/TheThingsNetwork/arduino-device-lib
+
 #include <TheThingsNetwork.h>
 
 // Set your AppEUI and AppKey
 const char *appEui = "70B3D57ED0022E0E";
-const char *appKey = "3D8E1D43DDD517D9DF89C1825B3F76EA";
+const char *appKey = "409945AA983522B340F08BAEAD0C1529";
 
 #define loraSerial Serial1
 #define debugSerial Serial
@@ -11,6 +13,8 @@ const char *appKey = "3D8E1D43DDD517D9DF89C1825B3F76EA";
 #define freqPlan TTN_FP_EU868
 
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
+
+//the typedef code comes from: https://github.com/brady-aiello/Seeeduino_LoRaWAN_for_hybrid_gateways/blob/master/Seeeduino-LoRaWAN-GPS-app/Seeeduino-LoRaWAN-GPS-app.ino
 
 typedef union {
   float f[2];               // Assigning fVal.f will also populate fVal.bytes;
@@ -40,13 +44,19 @@ void loop()
 {
   debugSerial.println("-- LOOP");
 
+  long randNumberLong = random(-90000000, 90000000);
+  long randNumberLat = random(-18000000, 180000000);
+
+  //latlong.f[0] = randNumberLat / 100000;
+  //latlong.f[1] = randNumberLong / 100000;
+
   double xCoord = 56.234464;
   double yCoord = 23.345908;
   // Prepare payload of 1 byte to indicate LED status
-  payload.f[0] = xCoord;
-  payload.f[1] = yCoord;
+  payload.f[0] = randNumberLat; //n to s
+  payload.f[1] = randNumberLong; //w to o
 
-  ttn.sendBytes(payload.Bytes, sizeof(payload));
+  ttn.sendBytes(payload.bytes, sizeof(payload));
   for (int i = 0; i < 8; i++) {
     debugSerial.print(payload.bytes[i], HEX);
     debugSerial.print(" ");
