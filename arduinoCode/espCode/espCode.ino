5c26c51f4920f20587116e88d2ec117988fb8ada
BoasKaken <boas.kalma@planet.nl>
2019-11-20 16:13:57 +0100

    modified:   arduinoCode/espCode/espCode.ino

--- arduinoCode/espCode/espCode.ino
+++ arduinoCode/espCode/espCode.ino
@@ -33,3 +33,17 @@ void checkNetwork(int networksFound) {
   }
 
 }
+
+/* output
+ *  2: Ziggo not the one 
+3: Ziggo not the one 
+4: TP-Link_E03C not the one 
+5: Ziggo3145224 not the one 
+6: VFNL-AED68A not the one 
+7: Harderwijk43 not the one 
+8: ZyXELA0C9FF not the one 
+9: H369A34959B not the one 
+10: TW237 2.4GHz not the one 
+11: Defqon 1 (d-word for pass) the one 
+12: HZN246298710 not the one 
+ */
