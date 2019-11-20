30f9c193659c72e3e68101e3d38995eaa49a9bc5
BoasKaken <boas.kalma@planet.nl>
2019-11-20 17:13:42 +0100

    modified:   arduinoCode/espCode/espCode.ino

--- arduinoCode/espCode/espCode.ino
+++ arduinoCode/espCode/espCode.ino
@@ -1,5 +1,10 @@
-const char* ssid = "dierentuinWifiNaam";
+#define uS_TO_S_FACTOR 1000000  //Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP 5 // in seconds
+
+char* CheckSsid = "Defqon 1 (d-word for pass)";
+
 boolean debug = true;
+bool inBounds = true;
 
 #include "ESP8266WiFi.h"
 
@@ -8,10 +13,12 @@ void setup() {
   if (debug) {
     Serial.begin(115200);
   }
+  while (!Serial) {}
+  Serial.println("startup");
 
   WiFi.mode(WIFI_STA);
 
-  delay(100);
+
 
 }
 
@@ -19,31 +26,32 @@ void loop() {
   // put your main code here, to run repeatedly:
   WiFi.disconnect();
   WiFi.scanNetworksAsync(checkNetwork);
+  if (inBounds) {
+    Serial.println("ik na naar bed");
+    ESP.deepSleep(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  } else {
+    while (true) {
+      Serial.println("De vogel is gejat");
+      delay(500);
+    }
+  }
 
-  //deep sleep
-  delay(20000);
+
 }
 
 
 void checkNetwork(int networksFound) {
   // scan for nearby networks:
-  for (int i = 0; i < networksFound; i++)
-  {
-    Serial.printf("%d: %s %s \n", i + 1, WiFi.SSID(i).c_str(), WiFi.SSID(i).equals("Defqon 1 (d-word for pass)") ? "the one" : "not the one");
+  bool networkFound = false;
+  for (int i = 0; i < networksFound; i++)  {
+    //Serial.printf("%d: %s %s \n", i + 1, WiFi.SSID(i).c_str(), WiFi.SSID(i).equals(CheckSsid) ? "the one" : "not the one");
+    if (WiFi.SSID(i).equals(CheckSsid)) {
+      networkFound = true;
+      break;
+    }
+  }
+  if (!networkFound) {
+    inBounds = false;
   }
 
 }
-
-/* output
- *  2: Ziggo not the one 
-3: Ziggo not the one 
-4: TP-Link_E03C not the one 
-5: Ziggo3145224 not the one 
-6: VFNL-AED68A not the one 
-7: Harderwijk43 not the one 
-8: ZyXELA0C9FF not the one 
-9: H369A34959B not the one 
-10: TW237 2.4GHz not the one 
-11: Defqon 1 (d-word for pass) the one 
-12: HZN246298710 not the one 
- */
