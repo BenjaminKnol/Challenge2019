adcc9ec8b4892d7e78ffb2bc3f44c513692cfe61
Benjamin <B.T.Knol@student.tudelft.nl>
2020-01-16 09:54:01 +0100

    update font awesome

--- front_end_laravel/birdstay/resources/views/birds/index.blade.php
+++ front_end_laravel/birdstay/resources/views/birds/index.blade.php
@@ -4,7 +4,7 @@
     <div class="w3-row-padding w3-margin-bottom">
         <div class="w3-quarter">
             <div class="w3-container w3-red w3-padding-16">
-                <div class="w3-left"><i class="fa fa-twitter w3-xxxlarge"></i></div>
+                <div class="w3-left"><i class="fab fa-twitter fa-3x"></i></div>
                 <div class="w3-right">
                     <h3>{{ $birds->count() }}</h3>
                 </div>
