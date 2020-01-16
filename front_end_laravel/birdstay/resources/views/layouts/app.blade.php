1fc4796724ae82d6265ae117a6726557a3ef4129
BoasKaken <boas.kalma@planet.nl>
2020-01-16 12:09:35 +0100

    to make create bird look better

--- front_end_laravel/birdstay/resources/views/layouts/app.blade.php
+++ front_end_laravel/birdstay/resources/views/layouts/app.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.topBar')
 
-@yield('content')
 
-
+<div align="center" style='position:  relative; top:55px;'>
+    <div class="w3-row-padding w3-margin-bottom">
+        <div class="w3-quarter" align="center">
+            <div class="w3-container w3-red w3-padding-16">
+                <div align="center">
+                    <div class="card" align="center">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- front_end_laravel/birdstay/resources/views/layouts/topBar.blade.php
+++ front_end_laravel/birdstay/resources/views/layouts/topBar.blade.php
@@ -1,38 +1,25 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<html style="height: 100%">
+<title>@yield('title', 'Birdstay')</title>
 
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/9e8d2c6531.js" crossorigin="anonymous"></script>
 
 <style>
     html, body, h1, h2, h3, h4, h5 {
         font-family: "Raleway", sans-serif
     }
-
 </style>
 
-<head>
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
-<body>
+@yield('style')
+<body class="w3-light-grey" style="height: 100%">
 <div id="app">
 
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
