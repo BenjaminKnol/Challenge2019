be99a4bcc265b5ff2c41b7d778005dfa69394c88
Benjamin <B.T.Knol@student.tudelft.nl>
2020-01-22 22:04:38 +0100

    set up email-npotifications when a bird goes missing

---
+++ front_end_laravel/birdstay/app/Notifications/BirdMissing.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class BirdMissing extends Notification
+{
+    use Queueable;
+
+    private $bird;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Bird $bird)
+    {
+        $this->bird = $bird;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Missing bird!')
+                    ->greeting('One of your birds requires help')
+                    ->line('A bird has gone missing')
+                    ->action('To the website', url("/locations/$this->bird"))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
